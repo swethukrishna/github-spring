f5044124ab4611fee056b0656f0adc278d010440
Anxo Gómez <anxogomez@gmail.com>
2017-10-29 21:04:17 +0100

    add unit test file

---
+++ src/test/java/com/mkyong/mvc/ConfigurationTest.java
@@ -0,0 +1,14 @@
+package com.mkyong.mvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConfigurationTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
